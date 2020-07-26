5e1c84c386733f363b3bf658dc8d53ac35421c81
Nicolas Froidure <nicolas.froidure@gmail.com>
2020-07-26 15:28:39 +0200

    feat(content): add representatives

--- pages/a_propos/nos_elus.js
+++ pages/a_propos/nos_elus.js
@@ -4,13 +4,55 @@ import MainContent from "../../components/mainContent";
 import { lightTextContent } from "../../styles";
 
 export default () => (
-  <Layout title="Nos élus et candidats">
+  <Layout title="Nos élu-es et candidat-es">
     <MainContent>
-      <h1>Nos élus et candidats</h1>
+      <h1>Nos élu-es et candidat-es</h1>
+      <h2>Stéphanie Stiernon</h2>
+      <ul>
+        <li>
+          <strong>Depuis 2020 :</strong> 3e adjointe au maire de la Ville de
+          Douai. Délégation : Urbanisme
+        </li>
+      </ul>
+      <h2>Jean-Christophe Leclercq</h2>
+      <ul>
+        <li>
+          <strong>Depuis 2020 :</strong> 8e adjoint au maire de la Ville de
+          Douai. Délégation : Cadre de vie et Condition Animale.
+        </li>
+      </ul>
+      <h2>Marie Delattre</h2>
+      <ul>
+        <li>
+          <strong>Depuis 2020 :</strong> 13e adjointe au maire de la Ville de
+          Douai. Délégation : Quartier
+        </li>
+      </ul>
+      <h2>Guy Caruyer</h2>
+      <ul>
+        <li>
+          <strong>Depuis 2020 :</strong> Conseiller Municipal à la Ville de
+          Douai. Alimentation et agriculture durables, quartier de Frais-Marais.
+        </li>
+      </ul>
+      <h2>Katia Bittner</h2>
+      <ul>
+        <li>
+          <strong>Depuis 2020 :</strong> Conseillère Municipale à la Ville de
+          Douai. Délégation : Prévention, santé et sport bien-être.
+        </li>
+      </ul>
+      <h2>Yves Piquot</h2>
+      <ul>
+        <li>
+          <strong>Depuis 2020 :</strong> Conseiller Municipal à la Ville de
+          Douai. Délégation : Biodiversité et adaptation au changement climatique.
+        </li>
+      </ul>
       <h2>Lucile Wacheux</h2>
       <ul>
         <li>
-          <strong>Depuis 2014 :</strong> 12e adjointe au maire de la Ville de
+          <strong>De 2014 à 2020 :</strong> 12e adjointe au maire de la Ville de
           Douai. Délégation : Citoyenneté - Jumelage
         </li>
         <li>
